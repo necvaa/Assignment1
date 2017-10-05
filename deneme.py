73ac82ee07aec1977230306f500284cf046147da
necvaa <necva@cs.hacettepe.edu.tr>
2017-10-05 09:49:08 +0300

    first example

---
+++ deneme.py
@@ -0,0 +1,6 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+x=[i for i in range(1001)]
+y=[math.sin(i) for i in range(1001)]
+plot(x,y)
